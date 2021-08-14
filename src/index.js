591a4659f1c9762fd4ede89326bb150b44a7d937
Isaac Cruz <isaac.clm1@gmail.com>
2021-08-14 18:12:22 -0300

    Prepara Deploy

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import store from './Redux/store';
 
 ReactDOM.render(
   <Provider store={ store }>
-    <BrowserRouter>
+    <BrowserRouter basename={ process.env.PUBLIC_URL }>
       <App />
     </BrowserRouter>
   </Provider>,
